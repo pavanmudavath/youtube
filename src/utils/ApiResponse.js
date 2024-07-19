ccfeb76d9e121dcca7335643b5cc05b1e8c1d8cf
pavanmudavath <mudavathpavankumar822@gmail.com>
2024-07-19 17:47:38 +0530

    user and video schema

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(statusCode, data, message="Succedd"){
+        this.statusCode=statusCode
+        this.data=data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+
+//or
+
+// const asyncHandler = (fn) => async() => {
+//     try {
+//         await fn(req,res,next)
+        
+//     } catch (error) {
+//         resizeBy.status(err.code || 500).josn({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
+
